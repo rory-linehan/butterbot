3feb754dd351af5a0d4dfd4d880d3b5102df5396
Rory Linehan <rorylinehan@protonmail.com>
2023-02-17 09:57:32 -0600

    update build script with optional repo logic

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t jyro-0:5000/butterbot:latest . && \
-docker push jyro-0:5000/butterbot:latest && \
-kubectl -n jyro run --image jyro-0:5000/butterbot:latest butterbot-test
+repo=${1:-}
+
+docker build -t ${repo}butterbot:latest .
+if [ ! -z "${repo}" ]; then
+  docker push ${repo}butterbot:latest
+fi
+kubectl -n jyro run --image ${repo}butterbot:latest butterbot-test
